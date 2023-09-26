50236a44e7ae38448edd607d222aa6cc058b090d
Kj Carter <kobecarter00@gmail.com>
2023-09-25 19:04:41 -0600

    commit

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,21 +1,50 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import axios from 'axios'
 import Character from './Character'
+
 
 const urlPlanets = 'http://localhost:9009/api/planets'
 const urlPeople = 'http://localhost:9009/api/people'
 
 function App() {
   // ❗ Create state to hold the data from the API
+const [planet, setPlanet] = useState("")
+const [characters, setCharacters] = useState("")
+
   // ❗ Create effects to fetch the data and put it in state
+  useEffect(() => {
+    axios.get(urlPeople)
+    .then(res =>{
+      setCharacters(res.data);
+    })
+    .catch(err => {
+      console.log('no characters found')
+    });
+  });
+
+  axios.get(urlPlanets)
+  .then(response => {
+    setPlanets(response.data);
+  })
+  .catch(error => {
+    console.log('Error fetching planets:', error);
+  });
+;
+
   return (
     <div>
       <h2>Star Wars Characters</h2>
-      <p>See the README of the project for instructions on completing this challenge</p>
+      {characters.map(character => {
+        const characterPlanet = planets.find(planet => planet.id === character.homeworld);
+
+        return (
+          <Character key={character.id} character={character} planet={characterPlanet} />
+        );
+      })}
       {/* ❗ Map over the data in state, rendering a Character at each iteration */}
     </div>
   )
-}
+
 
 export default App
 
--- frontend/components/Character.js
+++ frontend/components/Character.js
@@ -1,12 +1,22 @@
 import React from 'react'
 
-function Character() { // ❗ Add the props
+function Character({Character, planet}) { // ❗ Add the props
   // ❗ Create a state to hold whether the homeworld is rendering or not
   // ❗ Create a "toggle" click handler to show or remove the homeworld
+  const [showPlanet, setShowPlanet] = useState(false);
+
+  const togglePlanet = () => {
+    setShowPlanet(prevShowPlanet => !prevShowPlanet);
+  };
   return (
-    <div>
-      {/* Use the same markup with the same attributes as in the mock */}
-    </div>
+    <div className="character-card" onClick={togglePlanet}>
+    <div className="character-name">{character.name}</div>
+    {showPlanet && (
+      <div className="character-planet">
+        {planet ? `Homeworld: ${planet.name}` : 'Planet information not available'}
+      </div>
+    )}
+  </div>
   )
 }
 
